b6f4fa41c6e89482953e2021fb77d2a732e4162c
asomwaru <asomwaru01@gmail.com>
2021-09-25 22:36:22 -0400

    Can change board now. Need to fix merging cells

--- src/public/game.js
+++ src/public/game.js
@@ -1,218 +1,185 @@
-
 //Swipe right
-function shiftRight(grid)
-{
-  for (let i = 0; i < 16; i++)
-  {
-    if(i % 4 == 0)
-    {
-      let totalOne = grid[i]
-      let totalTwo = grid[i + 1]
-      let totalThree = grid[i + 2]
-      let totalFour = grid[i + 3]
-      let row = [totalOne, totalTwo, totalThree, totalFour]
+export function shiftRight(grid) {
+  for (let i = 0; i < 16; i++) {
+    if (i % 4 == 0) {
+      let totalOne = grid[i];
+      let totalTwo = grid[i + 1];
+      let totalThree = grid[i + 2];
+      let totalFour = grid[i + 3];
+      let row = [totalOne, totalTwo, totalThree, totalFour];
 
-      let filteredRow = row.filter(num => num)
-      let missing = 4 - filteredRow.length
-      let zeroes = Array(missing).fill(0)
-      let newRow = zeroes.concat(filteredRow)
+      let filteredRow = row.filter((num) => num);
+      let missing = 4 - filteredRow.length;
+      let zeroes = Array(missing).fill(0);
+      let newRow = zeroes.concat(filteredRow);
 
-      grid[i] = newRow[0]
-      grid[i + 1] = newRow[1]
-      grid[i + 2] = newRow[2]
-      grid[i + 3] = newRow[3]
+      grid[i] = newRow[0];
+      grid[i + 1] = newRow[1];
+      grid[i + 2] = newRow[2];
+      grid[i + 3] = newRow[3];
     }
   }
 }
 
 //Swipe left
-function shiftLeft(grid)
-{
-  for (let i = 0; i < 16; i++)
-  {
-    if(i % 4 == 0)
-    {
-      let totalOne = grid[i]
-      let totalTwo = grid[i + 1]
-      let totalThree = grid[i + 2]
-      let totalFour = grid[i + 3]
-      let row = [totalOne, totalTwo, totalThree, totalFour]
+export function shiftLeft(grid) {
+  for (let i = 0; i < 16; i++) {
+    if (i % 4 == 0) {
+      let totalOne = grid[i];
+      let totalTwo = grid[i + 1];
+      let totalThree = grid[i + 2];
+      let totalFour = grid[i + 3];
+      let row = [totalOne, totalTwo, totalThree, totalFour];
 
-      let filteredRow = row.filter(num => num)
-      let missing = 4 - filteredRow.length
-      let zeroes = Array(missing).fill(0)
-      let newRow = filteredRow.concat(zeroes)
+      let filteredRow = row.filter((num) => num);
+      let missing = 4 - filteredRow.length;
+      let zeroes = Array(missing).fill(0);
+      let newRow = filteredRow.concat(zeroes);
 
-      grid[i] = newRow[0]
-      grid[i + 1] = newRow[1]
-      grid[i + 2] = newRow[2]
-      grid[i + 3] = newRow[3]
+      grid[i] = newRow[0];
+      grid[i + 1] = newRow[1];
+      grid[i + 2] = newRow[2];
+      grid[i + 3] = newRow[3];
     }
   }
 }
 
-function combineRow(grid)
-{
-  for (let i = 0; i < 15; i++)
-  {
-    if (i != 3 && i != 7 && i != 11)
-    {
-      if (grid[i] == grid[i + 1])
-      {
-        let combinedTotal = grid[i] + grid[i + 1]
-        grid[i] = combinedTotal
-        grid[i + 1] = 0
+export function combineRow(grid) {
+  for (let i = 0; i < 15; i++) {
+    if (i != 3 && i != 7 && i != 11) {
+      if (grid[i] == grid[i + 1]) {
+        let combinedTotal = grid[i] + grid[i + 1];
+        grid[i] = combinedTotal;
+        grid[i + 1] = 0;
       }
     }
   }
 }
 
 //Swipe down
-function shiftDown(grid)
-{
-  for (let i = 0; i < 4; i++)
-  {
-    let totalOne = grid[i]
-    let totalTwo = grid[i + 4]
-    let totalThree = grid[i + 8]
-    let totalFour = grid[i + 12]
-    let column = [totalOne, totalTwo, totalThree, totalFour]
+export function shiftDown(grid) {
+  for (let i = 0; i < 4; i++) {
+    let totalOne = grid[i];
+    let totalTwo = grid[i + 4];
+    let totalThree = grid[i + 8];
+    let totalFour = grid[i + 12];
+    let column = [totalOne, totalTwo, totalThree, totalFour];
 
-    let filteredColumn = column.filter(num => num)
-    let missing = 4 - filteredColumn.length
-    let zeroes = Array(missing).fill(0)
-    let newColumn = zeroes.concat(filteredColumn)
+    let filteredColumn = column.filter((num) => num);
+    let missing = 4 - filteredColumn.length;
+    let zeroes = Array(missing).fill(0);
+    let newColumn = zeroes.concat(filteredColumn);
 
-    grid[i] = newColumn[0]
-    grid[i + 4] = newColumn[1]
-    grid[i + 8] = newColumn[2]
-    grid[i + 12] = newColumn[3]
+    grid[i] = newColumn[0];
+    grid[i + 4] = newColumn[1];
+    grid[i + 8] = newColumn[2];
+    grid[i + 12] = newColumn[3];
   }
 }
 
 //Swipe up
-function shiftUp(grid)
-{
-  for (let i = 0; i < 4; i++)
-  {
-    let totalOne = grid[i]
-    let totalTwo = grid[i + 4]
-    let totalThree = grid[i + 8]
-    let totalFour = grid[i + 12]
-    let column = [totalOne, totalTwo, totalThree, totalFour]
+export function shiftUp(grid) {
+  for (let i = 0; i < 4; i++) {
+    let totalOne = grid[i];
+    let totalTwo = grid[i + 4];
+    let totalThree = grid[i + 8];
+    let totalFour = grid[i + 12];
+    let column = [totalOne, totalTwo, totalThree, totalFour];
 
-    let filteredColumn = column.filter(num => num)
-    let missing = 4 - filteredColumn.length
-    let zeroes = Array(missing).fill(0)
-    let newColumn = filteredColumn.concat(zeroes)
+    let filteredColumn = column.filter((num) => num);
+    let missing = 4 - filteredColumn.length;
+    let zeroes = Array(missing).fill(0);
+    let newColumn = filteredColumn.concat(zeroes);
 
-    grid[i] = newColumn[0]
-    grid[i + 4] = newColumn[1]
-    grid[i + 8] = newColumn[2]
-    grid[i + 12] = newColumn[3]
+    grid[i] = newColumn[0];
+    grid[i + 4] = newColumn[1];
+    grid[i + 8] = newColumn[2];
+    grid[i + 12] = newColumn[3];
   }
 }
 
-
-function combineColumn(grid)
-{
-  for (let i = 0; i < 12; i++)
-  {
-    if (grid[i] == grid[i + 4])
-    {
-      let combinedTotal = grid[i] + grid[i + 4]
-      grid[i] = combinedTotal
-      grid[i + 4] = 0
+export function combineColumn(grid) {
+  for (let i = 0; i < 12; i++) {
+    if (grid[i] == grid[i + 4]) {
+      let combinedTotal = grid[i] + grid[i + 4];
+      grid[i] = combinedTotal;
+      grid[i + 4] = 0;
     }
   }
 }
 
 //Checks if moves are still available
-function movesLeft(grid)
-{
-  if (checkRight(grid) || checkLeft(grid) || checkUp(grid) || checkDown(grid))
-  {
-    return true
+export function movesLeft(grid) {
+  if (checkRight(grid) || checkLeft(grid) || checkUp(grid) || checkDown(grid)) {
+    return true;
   }
-  return false
+  return false;
 }
 
-function checkRight(grid)
-{
-  let tempGrid = []
-  let testGrid = tempGrid.concat(grid)
+export function checkRight(grid) {
+  let tempGrid = [];
+  let testGrid = tempGrid.concat(grid);
 
-  shiftRight(testGrid)
-  combineRow(testGrid)
-  shiftRight(testGrid)
+  shiftRight(testGrid);
+  combineRow(testGrid);
+  shiftRight(testGrid);
 
-  for (let i = 0; i < 15; i++)
-  {
-    if (grid[i] != testGrid[i])
-    {
-      return false
+  for (let i = 0; i < 15; i++) {
+    if (grid[i] != testGrid[i]) {
+      return false;
     }
   }
 
-  return true
+  return true;
 }
 
-function checkLeft(grid)
-{
-  let tempGrid = []
-  let testGrid = tempGrid.concat(grid)
+export function checkLeft(grid) {
+  let tempGrid = [];
+  let testGrid = tempGrid.concat(grid);
 
-  shiftLeft(testGrid)
-  combineRow(testGrid)
-  shiftLeft(testGrid)
+  shiftLeft(testGrid);
+  combineRow(testGrid);
+  shiftLeft(testGrid);
 
-  for (let i = 0; i < 15; i++)
-  {
-    if (grid[i] != testGrid[i])
-    {
-      return false
+  for (let i = 0; i < 15; i++) {
+    if (grid[i] != testGrid[i]) {
+      return false;
     }
   }
 
-  return true
+  return true;
 }
 
-function checkUp(grid)
-{
-  let tempGrid = []
-  let testGrid = tempGrid.concat(grid)
+export function checkUp(grid) {
+  let tempGrid = [];
+  let testGrid = tempGrid.concat(grid);
 
-  shiftUp(testGrid)
-  combineColumn(testGrid)
-  shiftUp(testGrid)
+  shiftUp(testGrid);
+  combineColumn(testGrid);
+  shiftUp(testGrid);
 
-  for (let i = 0; i < 15; i++)
-  {
-    if (grid[i] != testGrid[i])
-    {
-      return false
+  for (let i = 0; i < 15; i++) {
+    if (grid[i] != testGrid[i]) {
+      return false;
     }
   }
 
-  return true
+  return true;
 }
 
-function checkDown(grid)
-{
-  let tempGrid = []
-  let testGrid = tempGrid.concat(grid)
+export function checkDown(grid) {
+  let tempGrid = [];
+  let testGrid = tempGrid.concat(grid);
 
-  shiftDown(testGrid)
-  combineColumn(testGrid)
-  shiftDown(testGrid)
+  shiftDown(testGrid);
+  combineColumn(testGrid);
+  shiftDown(testGrid);
 
-  for (let i = 0; i < 15; i++)
-  {
-    if (grid[i] != testGrid[i])
-    {
-      return false
+  for (let i = 0; i < 15; i++) {
+    if (grid[i] != testGrid[i]) {
+      return false;
     }
   }
 
-  return true
+  return true;
 }
